a303fecd34a6b4ef79561a8c58b5cf35722882b2
WalkWithWind <956035125@qq.com>
2018-06-08 21:54:47 +0800

    Initial Commit

---
+++ AuthView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AuthView
+//
+//  Created by mac on 2018/6/8.
+//  Copyright © 2018年 ShiBiao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
